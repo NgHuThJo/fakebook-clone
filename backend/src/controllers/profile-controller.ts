4a674ecd95cc7da06ebca8cbd05fb29daaf8351f
John Doe <nghuthjo@gmail.com>
2024-06-27 15:42:06 +0200

    Refactor aggregation pipeline of feed

--- backend/src/controllers/profile-controller.ts
+++ backend/src/controllers/profile-controller.ts
@@ -26,25 +26,59 @@ const feedPipeline = async () => {
         from: "posts",
         localField: "post",
         foreignField: "_id",
+        as: "post",
         pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "author",
+              foreignField: "_id",
+              as: "author",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    email: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $unwind: "$author",
+          },
+          {
+            $addFields: {
+              created: {
+                $dateToString: {
+                  format: "%m/%d/%Y",
+                  date: "$created",
+                },
+              },
+            },
+          },
           {
             $project: {
               __v: 0,
             },
           },
         ],
-        as: "post",
       },
     },
     {
-      $unwind: {
-        path: "$post",
+      $unwind: "$post",
+    },
+    {
+      $project: {
+        __v: 0,
       },
     },
   ]);
 
   return result;
 };
+
+feedPipeline();
 
 export const getFeeds: RequestHandler[] = [
   async (req, res, next) => {
--- frontend/src/features/profile/components/newsfeed/newsfeed.tsx
+++ frontend/src/features/profile/components/newsfeed/newsfeed.tsx
@@ -1,12 +1,28 @@
 import { Image } from "@/components/ui/image";
+import { formatDate } from "@/utils/intl";
 import styles from "./newsfeed.module.css";
 import { thumbs_up_icon } from "@/assets/images/icons";
 
-type FeedData = {
-  post: string;
+type UserData = {
+  _id: string;
+  username: string;
+  email: string;
+};
+
+type PostData = {
+  author: UserData;
+  comments: PostData[];
   created: string;
-  imgUrl: string;
   likesCount: number;
+  _id: string;
+  post: string;
+  title: string;
+};
+
+type FeedData = {
+  _id: string;
+  post: PostData;
+  imgUrl: string;
 };
 
 type NewsfeedProps = React.PropsWithoutRef<{ feedData: FeedData[] }>;
@@ -17,12 +33,12 @@ export function Newsfeed({ feedData }: NewsfeedProps) {
       {feedData.map((feed, index) => (
         <li className={styles.feed} key={index}>
           <Image src={feed.imgUrl}></Image>
-          <p>{feed.post}</p>
+          <p>{feed.post.post}</p>
           <div>
-            <p>Created: {feed.created}</p>
+            <p>Created: {feed.post.created}</p>
             <button className={styles.likes}>
               <Image className="icon" src={thumbs_up_icon}></Image>
-              <p>{feed.likesCount}</p>
+              <p>{feed.post.likesCount}</p>
             </button>
           </div>
         </li>
